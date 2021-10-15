cba2b6c24ec108676300d93148e9feb2402ccfe7
Wiku Galindra Wardhana <wikugw@gmail.com>
2021-10-15 19:55:49 +0700

    add firebase core dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
+void main() async {
+  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
