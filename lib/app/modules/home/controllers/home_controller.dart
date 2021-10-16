bd08cb8ef70d3643e1e3426e82c0493f2544fd94
Wiku Galindra Wardhana <wikugw@gmail.com>
2021-10-16 18:20:34 +0700

    get user data pada home view

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:money_monitoring/app/modules/home/data/models/user_model.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  var user = UserModel().obs;
+  MoneyHistory currentMonthRecord = MoneyHistory();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  Future<UserModel> getCurrentMonthRecord(String loggedInEmail) async {
+    CollectionReference users = firestore.collection('users');
+
+    var dateNow = DateTime.now();
+
+    String monthNumber = DateFormat.M().format(dateNow);
+    String year = DateFormat.y().format(dateNow);
+
+    final currentUser = await users.doc(loggedInEmail).get();
+    final currentUserData = currentUser.data() as Map<String, dynamic>;
+    final moneyHistoryRef = await users
+        .doc(loggedInEmail)
+        .collection('moneyHistory')
+        .where('month', isEqualTo: monthNumber)
+        .where('year', isEqualTo: year)
+        .get();
+
+    List<MoneyHistory> moneyHistoryList = [];
+    if (moneyHistoryRef.docs.length > 0) {
+      moneyHistoryRef.docs.forEach((element) {
+        var moneyHistory = element.data();
+        moneyHistoryList.add(MoneyHistory(
+          year: moneyHistory['year'],
+          month: moneyHistory['month'],
+          monthName: moneyHistory['monthName'],
+          totalInMonth: moneyHistory['totalInMonth'],
+        ));
+      });
+    }
+
+    user(UserModel.fromJson(currentUserData));
+    user.refresh();
+
+    user.update((val) {
+      val!.moneyHistory = moneyHistoryList;
+    });
+
+    user.value.moneyHistory?.forEach((element) {
+      if (element.month == monthNumber && element.year == year) {
+        currentMonthRecord = element;
+      }
+    });
+
+    return user.value;
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:money_monitoring/app/controllers/auth_controller.dart';
+import 'package:money_monitoring/app/modules/home/data/models/user_model.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  final authC = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+    return FutureBuilder<UserModel>(
+        future: controller.getCurrentMonthRecord(authC.user.value.email!),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            var moneyHistoryList = snapshot.data!.moneyHistory;
+            print(controller.currentMonthRecord.monthName);
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('HomeView'),
+                centerTitle: true,
+              ),
+              body: Center(
+                child: Text(
+                  'HomeView is working',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        });
   }
 }
